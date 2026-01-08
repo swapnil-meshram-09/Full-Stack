4a1968c9ff1fd8f89470b8ac272cb516c813062b
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-08 19:27:53 +0530

    new commit

---
+++ Full-Stack/frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  // useEffect(() =>{
+  // (async function func(){
+  //     const response = await axios.get('/api/data')
+  //     console.log(response.data)
+  //     setData(response.data)
+  // })()
+  // },[])
+
+
+  useEffect(() =>{
+    axios.get('/api/data')
+    .then((response) =>{    
+        console.log(response.data);
+
+        setData(response.data)   
+    })
+    .catch((error) =>{
+        console.log(error);   
+    })
+  },[])
+
+  return (
+    <>
+    <div>
+      <h3>Data Fetching . . . </h3>
+    </div>
+    <div>
+        {data.map((data) =>(
+          <div key={data.id}>
+             {data.title} 
+             <br />
+             {data.description}
+          </div>
+        ))}
+    </div>
+    </>
+  )
+}
+
+export default App
