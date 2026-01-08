d92710d27c0121db2927a758fe122e6070a38cdd
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-08 19:32:57 +0530

    new commit

--- Full-Stack/backend/server.js
+++ Full-Stack/backend/server.js
@@ -24,7 +24,15 @@ app.get('/api/data', (req, res) =>{
     //     res.send('server is starting.')
     // },3000)
 
-    res.send(Data)
+    // res.send(Data)
+
+    if(req.query.search){
+        const filterdData = Data.filter((data) =>{
+            data.title.includes(req.query.search)
+            req.send(filterdData)
+            return
+        })
+    }
 })
 
 
