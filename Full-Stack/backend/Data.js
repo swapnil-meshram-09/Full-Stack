3eb509b58f6f73637c2cf37a12da9a20fdfff866
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-08 19:28:08 +0530

    new commit

---
+++ Full-Stack/backend/Data.js
@@ -0,0 +1,35 @@
+const Data = [
+    {
+        id:0,
+        title:'data',
+        description:'description'
+    },
+    {
+        id:1,
+        title:'data 1',
+        description:'description 1'
+    },
+    {
+        id:2,
+        title:'data 2',
+        description:'description 2'
+    },
+    {
+        id:3,
+        title:'data 3',
+        description:'description 3'
+    },
+    {
+        id:4,
+        title:'data 4',
+        description:'description 4'
+    },
+    {
+        id:5,
+        title:'data 5',
+        description:'description 5'
+    },
+    
+]
+
+export default Data
